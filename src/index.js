33f0be9bbe6b33c2d6b544cf56886acfbed2a064
Ivan Zhevakin <ixvakin@gmail.com>
2019-03-03 22:57:25 +0300

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.css'
 import App from './components/App/App'
 import * as serviceWorker from './serviceWorker'
 import configureStore from './redux/store'
