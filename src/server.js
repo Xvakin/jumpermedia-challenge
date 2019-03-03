4f9be4707e7b27cd570f18b60e72f6902c3fa396
Ivan Zhevakin <ixvakin@gmail.com>
2019-03-04 01:16:00 +0300

    add github api proxy

---
+++ src/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const Octokit = require('@octokit/rest')
+const parse = require('parse-link-header')
+const app = express()
+const port = process.env.PORT || 5000
+const octokit = new Octokit({
+  auth: `token ${process.env.GITHUB_API_TOKEN}`,
+})
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/api/*', (req, res) => {
+  const path = req.url.replace('/api', ``)
+  octokit.request('GET ' + path)
+    .then(({ data, headers }) => {
+      const pagination = parse(headers.link)
+      res.send({ data, pagination })
+    })
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
