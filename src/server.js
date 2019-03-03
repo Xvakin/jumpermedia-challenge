d31d934db5dfee48062ac0572ead6819d23034cb
Ivan Zhevakin <ixvakin@gmail.com>
2019-03-04 01:32:19 +0300

    change proxy port

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const Octokit = require('@octokit/rest')
 const parse = require('parse-link-header')
 const app = express()
-const port = process.env.PORT || 5000
+const port = 5000
 const octokit = new Octokit({
   auth: `token ${process.env.GITHUB_API_TOKEN}`,
 })
