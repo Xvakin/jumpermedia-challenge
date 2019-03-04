64e2eb0c259d7767fa9e416ab2ef3889347c251a
Ivan Zhevakin <ixvakin@gmail.com>
2019-03-04 03:36:09 +0300

    serve build static

--- src/server.js
+++ src/server.js
@@ -1,15 +1,22 @@
 require('dotenv').config()
 const express = require('express')
+const path = require('path')
 const bodyParser = require('body-parser')
 const Octokit = require('@octokit/rest')
 const parse = require('parse-link-header')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 const octokit = new Octokit({
   auth: `token ${process.env.GITHUB_API_TOKEN}`,
 })
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(express.static(path.join(__dirname, '..', 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
 
 app.get('/api/*', (req, res) => {
   const path = req.url.replace('/api', ``)
